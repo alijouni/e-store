e8b27f1d80fcd6baf313aa22c954cd8e1b749d2d
Ali Jouni <alijouni@outlook.de>
2022-04-01 23:37:13 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Route,Routes } from 'react-router-dom';
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/hats' element={<HatsPage/>}/>
+      </Routes>
+
     </div>
   );
 }
