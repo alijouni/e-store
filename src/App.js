2ce462a1ffcaee929f99244731bb6036d8612731
Ali Jouni <alijouni@outlook.de>
2022-05-23 12:56:25 +0300

    Redirect User to home and create Cart Icon

--- src/App.js
+++ src/App.js
@@ -1,64 +1,61 @@
-import React from 'react';
-import { Route,Routes } from 'react-router-dom';
-import {connect} from 'react-redux';
-import './App.css';
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument} from './firebase/firebase.utils';
-import {setCurrentUser} from './redux/user/user.actions'
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
   </div>
-)
+);
 
 const JacketsPage = () => (
   <div>
     <h1>JACKETS PAGE</h1>
   </div>
-)
+);
 const SneakersPage = () => (
   <div>
     <h1>SNEAKERS PAGE</h1>
   </div>
-)
+);
 const WomenPage = () => (
   <div>
     <h1>WOMEN PAGE</h1>
   </div>
-)
+);
 const MenPage = () => (
   <div>
     <h1>MEN PAGE</h1>
   </div>
-)
+);
 
 class App extends React.Component {
-
-
-  unsubscribeFromAuth=null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser}=this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        
-        userRef.onSnapshot(snapShot => {
+
+        userRef.onSnapshot((snapShot) => {
           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          })
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
+      } else {
+        setCurrentUser(userAuth);
       }
-      else {
-        setCurrentUser(userAuth)
-      }
-    })
+    });
   }
 
   componentWillUnmount() {
@@ -67,23 +64,37 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/shop/hats' element={<HatsPage />} />
-          <Route path='/shop/sneakers' element={<SneakersPage />} />
-          <Route path='/shop/jackets' element={<JacketsPage />} />
-          <Route path='/shop/women' element={<WomenPage />} />
-          <Route path='/shop/men' element={<MenPage />} />
-          <Route path='/shop/' element={<ShopPage />} />
-          <Route path='/signin/' element={<SignInAndSignUpPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop/hats" element={<HatsPage />} />
+          <Route path="/shop/sneakers" element={<SneakersPage />} />
+          <Route path="/shop/jackets" element={<JacketsPage />} />
+          <Route path="/shop/women" element={<WomenPage />} />
+          <Route path="/shop/men" element={<MenPage />} />
+          <Route path="/shop/" element={<ShopPage />} />
+          <Route
+            exact
+            path="/signin/"
+            element={
+              this.props.currentUser ? (
+                <Navigate to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Routes>
-
       </div>
     );
   }
 }
-const mapDispatchToProps=dispatch=>({
-  setCurrentUser:user=>dispatch(setCurrentUser(user))
-})
-export default connect(null,mapDispatchToProps)(App);
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import "./header.styles.scss";
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
+
 
 const Header = ({ currentUser }) => {
   return (
@@ -27,6 +29,7 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
