c75df3be22e5f6e6b1bc9ef9b8823a3c11d3538c
Ali Jouni <alijouni@outlook.de>
2022-05-25 23:33:24 +0300

    Cart dropdown and Add to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss'
 const CollectionItem = ({id, name, price , imageUrl}) => {
   return (
@@ -10,7 +12,8 @@ const CollectionItem = ({id, name, price , imageUrl}) => {
           <div className='collection-footer'>
               <span className='name'>{name}</span>
               <span className='price'>{price}</span>
-          </div>
+      </div>
+      <CustomButton inverted>Add to cart </CustomButton>
     </div>
   )
 }
