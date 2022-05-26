1f747c24f48dfab57d55dce2e0d94e6832aa3963
Ali Jouni <alijouni@outlook.de>
2022-05-26 23:32:00 +0300

    Updated shop data with new data, updated carto group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './custom-button.styles.scss'
+import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn,inverted,...otherProps}) => {
-    return (
-        <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}  {...otherProps}>
-            {children}
-      </button>
-  )
+function CustomButton({
+  children, isGoogleSignIn, inverted, ...otherProps
+}) {
+  return (
+    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+      {children}
+    </button>
+  );
 }
 
-export default CustomButton
+export default CustomButton;
