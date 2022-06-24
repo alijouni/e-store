76b66463d18c96373e7749057ea68e5feb42e15c
Ali Jouni <alijouni@outlook.de>
2022-06-24 23:57:54 +0300

    Adding spinner loading using HOC patterns

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,11 +4,20 @@ import { connect } from 'react-redux';
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../../components/collection/collection.component";
 
 import { firestore,convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview)
+const CollectionPageWithSpinner=WithSpinner(CollectionPage)
 class ShopPage extends React.Component{
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
   
   
@@ -19,16 +28,18 @@ class ShopPage extends React.Component{
     collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     })
   }
 
 
   render() {
+    const { loading } = this.state;
     return (
       <div className="shop-page">
         <Routes>
-          <Route exact path="/" element={<CollectionOverview />} />
-          <Route path=":collectionId" element={<CollectionPage />} />
+          <Route exact path="/" element={<CollectionOverviewWithSpinner isLoading={loading} />} />
+          <Route path=":collectionId" element={<CollectionPageWithSpinner isLoading={loading} />} />
         </Routes>
       </div>
     );
