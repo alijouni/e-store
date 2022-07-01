3153fee770f2228113192d43ccfd7fb24574b227
Ali Jouni <alijouni@outlook.de>
2022-07-01 15:17:53 +0300

    Add collection loaded selector to avoid null collection at collectionId routes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,21 +5,22 @@ import {createStructuredSelector} from 'reselect';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import {
+  selectIsCollectionFetching,
+  selectIsCollectionsLoaded,
+} from "../../redux/shop/shop.selectors";
 
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../../components/collection/collection.component";
 
 // import { firestore,convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview)
-const CollectionPageWithSpinner=WithSpinner(CollectionPage)
-class ShopPage extends React.Component{
-  
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+class ShopPage extends React.Component {
   componentDidMount() {
-
     const { fetchCollectionsStartAsync } = this.props;
     fetchCollectionsStartAsync();
     // const { updateCollections } = this.props;
@@ -32,32 +33,44 @@ class ShopPage extends React.Component{
     //   this.setState({ loading: false });
     // })
 
-    // //Promises 
+    // //Promises
     // collectionRef.get().then(snapshot => {
     //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
     //   updateCollections(collectionsMap);
     //   this.setState({ loading: false });
     // })
-
   }
 
-
   render() {
-    const { isCollectionsFetching } = this.props;
+    const { isCollectionsFetching, isCollectionsLoaded } = this.props;
     return (
       <div className="shop-page">
         <Routes>
-          <Route exact path="/" element={<CollectionOverviewWithSpinner isLoading={isCollectionsFetching} />} />
-          <Route path=":collectionId" element={<CollectionPageWithSpinner isLoading={isCollectionsFetching} />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <CollectionOverviewWithSpinner
+                isLoading={isCollectionsFetching}
+              />
+            }
+          />
+          <Route
+            path=":collectionId"
+            element={
+              <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} />
+            }
+          />
         </Routes>
       </div>
     );
+  }
 }
-};
 
 const mapStateToProps = createStructuredSelector({
-  isCollectionsFetching:selectIsCollectionFetching
-})
+  isCollectionsFetching: selectIsCollectionFetching,
+  isCollectionsLoaded: selectIsCollectionsLoaded,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStartAsync:()=>dispatch(fetchCollectionsStartAsync())
